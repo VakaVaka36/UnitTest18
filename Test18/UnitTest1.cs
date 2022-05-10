42d76be9cdf78b2aea12344e6556e04008f7bdff
Егор <@DESKTOP-3UQG7N8>
2022-05-10 21:27:20 +0300

    Добавьте файлы проекта.

---
+++ Test18/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Solostov18_10;
+
+namespace Test18
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        [TestMethod]
+        public void TestResultat1()
+        {
+            double x = 4;
+            double a = 1;
+            double b = 2;
+            double c = 2;
+
+            Assert.AreEqual(UnitTest.Resultat(a, x, b, c), "F=-18");
+        }
+
+        [TestMethod]
+        public void TestResultat2()
+        {
+            double x = 15;
+            double a = 10;
+            double b = 6;
+            double c = 0;
+
+            Assert.AreEqual(UnitTest.Resultat(a, x, b, c), "F=-∞");
+        }
+
+        [TestMethod]
+        public void TestResultat3()
+        {
+            double x = 5;
+            double a = 2;
+            double b = 1;
+            double c = -1;
+
+            Assert.AreEqual(UnitTest.Resultat(a, x, b, c), "F=5");
+        }
+
+        [TestMethod]
+        public void TestUslovie1()
+        {
+            Assert.IsTrue(UnitTest.Uslovie1(4, 1));
+        }
+
+        [TestMethod]
+        public void TestUslovie2()
+        {
+            Assert.IsTrue(UnitTest.Uslovie2(7, 0));
+        }
+    }
+}
---
+++ UnitTest18/UnitTestCode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Solostov18_10
+{
+
+    public class UnitTest
+    {
+        public static double var(string a)
+        {
+            Console.WriteLine("Введите " + a);
+            return Convert.ToDouble(Console.ReadLine());
+
+        }
+        public static bool Uslovie1 (double x, double c)
+        {
+            return x < 5 && c != 0;
+        }
+
+        public static bool Uslovie2 (double x, double c)
+        {
+            return (x > 5 && c == 0);
+        }
+        public static string Resultat (double a, double x, double b, double c)
+        {
+           if (Uslovie1(x, b))
+                return "F=" + Convert.ToString((-a)*(Math.Pow(x, 2))-b);
+           else if (Uslovie2(x, b))
+                return "F=" + Convert.ToDouble((x - a)/x); 
+           else
+                return "F=" + Convert.ToString(-x/c);
+
+        }   
+        static void Main (string[] args)
+        {
+            double a = var("a");
+            double b = var("b");
+            double c = var("c");
+            double x = var("x");
+            Resultat(a, b, c, x);
+        }
+    }
+}
